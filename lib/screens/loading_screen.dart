19a249a4984826bd5d9b02922eb9327d2fa8bfb7
rexsteroxy <rexsteroxy22@gmail.com>
2020-03-15 08:14:50 -0400

    imported the http pluging so as to consume some external weather data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as rex;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,14 +11,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = new Location();
     await location.getCurrentLocation();
-   print(location.latitude);
-   print(location.longitude);
+    print(location.latitude);
+    print(location.longitude);
+  }
 
+  void getData() async {
+    rex.Response response = await rex.get('http://samples.openweathermap.org/data/2.5/'
+        'weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
+if(response.statusCode != 200){
+  print(response.statusCode);
+}else{}
+    print(response.body);
   }
 
   void initState() {
     super.initState();
     getLocation();
+    getData();
   }
 
   @override
