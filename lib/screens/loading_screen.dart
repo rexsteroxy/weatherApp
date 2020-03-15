0079d4b97a68c1149f995e6dcac872d9789f7415
rexsteroxy <rexsteroxy22@gmail.com>
2020-03-15 09:13:07 -0400

    implemented jsondecode on the json data gotten

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as rex;
+import 'dart:convert';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,7 +12,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void getLocation() async {
     Location location = new Location();
     await location.getCurrentLocation();
-    print(location.latitude);
+    print('hello: ${location.latitude}');
     print(location.longitude);
   }
 
@@ -20,8 +21,23 @@ class _LoadingScreenState extends State<LoadingScreen> {
         'weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
 if(response.statusCode != 200){
   print(response.statusCode);
-}else{}
-    print(response.body);
+}else{
+  var data = response.body;
+//  var longitude = jsonDecode(data)['coord']['lon'];
+//  var latitude = jsonDecode(data)['coord']['lat'];
+//  var description = jsonDecode(data)['weather'][0]['description'];
+//  var humidity = jsonDecode(data)['main']['humidity'];
+  var jd = jsonDecode(data);
+  String cityName= jd['name'];
+  double temperature = jd['main']['temp'];
+  int condition = jd['weather'][0]['id'];
+//  print('Longitude from JSON: $longitude');
+//  print('Latitude from JSON: $latitude');
+  print('Description from JSON: $temperature');
+  print('CityName from JSON: $cityName');
+  print('Humidiy from JSON: $condition');
+}
+
   }
 
   void initState() {
