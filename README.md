8c4d0964e8c5d3cf4eb8233caa6d149854283da4
rexsteroxy <rexsteroxy22@gmail.com>
2020-03-15 09:33:39 -0400

    made an api call to openweatherapi organization

--- README.md
+++ README.md
@@ -9,3 +9,13 @@ A weather app for live weather data in the current location of the device as wel
 For help getting started with Flutter and deploying this app, view the
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+
+//  var longitude = jsonDecode(data)['coord']['lon'];
+//  var latitude = jsonDecode(data)['coord']['lat'];
+//  var description = jsonDecode(data)['weather'][0]['description'];
+//  var humidity = jsonDecode(data)['main']['humidity'];
+
+
+//  print('Longitude from JSON: $longitude');
+//  print('Latitude from JSON: $latitude');
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,49 +3,48 @@ import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as rex;
 import 'dart:convert';
 
+const apiKey = '0c90cc4ce053771a96d1f6f4094b20ce';
+
+
 class LoadingScreen extends StatefulWidget {
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  void getLocation() async {
-    Location location = new Location();
-    await location.getCurrentLocation();
-    print('hello: ${location.latitude}');
-    print(location.longitude);
-  }
-
-  void getData() async {
-    rex.Response response = await rex.get('http://samples.openweathermap.org/data/2.5/'
-        'weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22');
-if(response.statusCode != 200){
-  print(response.statusCode);
-}else{
-  var data = response.body;
-//  var longitude = jsonDecode(data)['coord']['lon'];
-//  var latitude = jsonDecode(data)['coord']['lat'];
-//  var description = jsonDecode(data)['weather'][0]['description'];
-//  var humidity = jsonDecode(data)['main']['humidity'];
-  var jd = jsonDecode(data);
-  String cityName= jd['name'];
-  double temperature = jd['main']['temp'];
-  int condition = jd['weather'][0]['id'];
-//  print('Longitude from JSON: $longitude');
-//  print('Latitude from JSON: $latitude');
-  print('Description from JSON: $temperature');
-  print('CityName from JSON: $cityName');
-  print('Humidiy from JSON: $condition');
-}
-
-  }
+  double latitude;
+  double longitude;
 
   void initState() {
     super.initState();
     getLocation();
+
+  }
+  void getLocation() async {
+    Location location = new Location();
+    await location.getCurrentLocation();
+    latitude =  location.latitude;
+    longitude =location.longitude;
     getData();
   }
+  void getData() async {
+    rex.Response response = await rex.get('https://api.openweathermap.org/data/2.5/'
+        'weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    if(response.statusCode != 200){
+      print(response.statusCode);
+    }
+    else{
+      var data = response.body;
+      var jd = jsonDecode(data);
+      String cityName= jd['name'];
+      double temperature = jd['main']['temp'];
+      int condition = jd['weather'][0]['id'];
+      print('Temperature from JSON: $temperature');
+      print('CityName from JSON: $cityName');
+      print('Condition from JSON: $condition');
+    }
 
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold();
